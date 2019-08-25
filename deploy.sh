09ee373fbb3743a37f5561ac701037c6de95028e
Flygar <w871546712@gmail.com>
2019-08-26 03:52:02 +0800

    backup site Mon Aug 26 03:52:02 CST 2019

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo -e "\033[0;32mFor blog-hugo...\033[0m"
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="backup site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+#---------------------------------------------------
+
+echo -e "\033[0;32mFor flygar.github.io ....\033[0m"
+# Build the project.
+hugo -t maupassant # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
