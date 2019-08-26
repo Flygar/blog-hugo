83ef0b8545499eb0aebc3fabb88f986ddb6e77ad
Flygar <w871546712@gmail.com>
2019-08-26 21:02:56 +0800

    backup site Mon Aug 26 21:02:56 CST 2019

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-echo -e "\033[0;32mFor blog-hugo...\033[0m"
-# rm -rf public
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="backup site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
 # git submodule add git@github.com:Flygar/flygar.github.io.git public
 #---------------------------------------------------
 
@@ -39,3 +23,20 @@ git push origin master
 
 # Come Back
 cd ..
+
+#-------------------------------------------------------
+
+echo -e "\033[0;32mFor blog-hugo...\033[0m"
+# rm -rf public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="backup site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
