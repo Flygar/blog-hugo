4d2e05075258a6e7c13a58848b99eb55485b96f7
Flygar <w871546712@gmail.com>
2019-08-26 16:48:06 +0800

    backup site Mon Aug 26 16:48:06 CST 2019

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo -e "\033[0;32mFor blog-hugo...\033[0m"
+# 
+rm -rf public
 # Add changes to git.
 git add -A
 
@@ -14,6 +16,7 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
+git submodule add git@github.com:Flygar/flygar.github.io.git public
 #---------------------------------------------------
 
 echo -e "\033[0;32mFor flygar.github.io ....\033[0m"
